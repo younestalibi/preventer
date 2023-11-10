6100a645b7e554dc63458447fb38bdd89c8a2633
younes talibi <younessetalibi11@gmail.com>
2023-11-10 17:44:17 +0100

    hi

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,9 @@
-// content.js
-// Function to handle the button click
 function handleAdSkipButtonClick() {
     console.log('Ad skip button clicked');
-    // Perform actions based on the ad skip button click
   }
-  
-  // Function to check for the existence of the ad skip button
   function checkForAdSkipButton() {
     const adSkipButton = document.querySelector('.ytp-ad-skip-button');
     if (adSkipButton) {
-      // The ad skip button exists, trigger the click event
       setTimeout(() => {
         adSkipButton.click();
       }, 500);
@@ -17,10 +11,8 @@ function handleAdSkipButtonClick() {
     }
   }
   
-  // Create a MutationObserver instance
   const observer = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
-      // Check for changes in the document (you can adjust this based on your needs)
       checkForAdSkipButton();
     });
   });
