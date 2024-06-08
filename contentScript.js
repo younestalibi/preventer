ea5fe3ac56ac5e320eed570e2a113097b045ccbe
younes talibi <younessetalibi11@gmail.com>
2024-06-09 00:32:27 +0100

    update skip

--- contentScript.js
+++ contentScript.js
@@ -1,24 +1,36 @@
-function handleAdSkipButtonClick() {
-    console.log('Ad skip button clicked');
-  }
-  function checkForAdSkipButton() {
-    // const adSkipButton = document.querySelector('.ytp-ad-skip-button');
-    const adSkipButton = document.querySelector(".ytp-ad-skip-button-modern")
-    if (adSkipButton) {
-      adSkipButton.click();
-      handleAdSkipButtonClick();
-    }
-  }
-  
-  const observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      checkForAdSkipButton();
+function checkForAdSkipButton() {
+  const adSkipButton1 = document.querySelector(".ytp-ad-skip-button");
+  const adSkipButton2 = document.querySelector(".ytp-ad-skip-button-modern");
+  if (adSkipButton1) {
+    adSkipButton1.click();
+  } else if (adSkipButton2) {
+    adSkipButton2.click();
+  } else {
+    let targetText = "التخطّي";
+    let elementsWithText = document.querySelectorAll(
+      ":contains(" + targetText + ")"
+    );
+
+    elementsWithText.forEach((element) => {
+      let parent = element;
+      while (parent && parent.tagName !== "button") {
+        parent = parent.parentElement;
+      }
+      if (parent && parent.tagName === "button") {
+        parent.click();
+      }
     });
+  }
+}
+
+const observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    checkForAdSkipButton();
   });
-  
-  // Start observing the document for changes
-  observer.observe(document, { childList: true, subtree: true });
-  
-  // Initial check for the ad skip button in case it already exists
-  checkForAdSkipButton();
-  
+});
+
+// Start observing the document for changes
+observer.observe(document, { childList: true, subtree: true });
+
+// Initial check for the ad skip button in case it already exists
+checkForAdSkipButton();
