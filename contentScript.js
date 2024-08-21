f2b351585440301d7197e92968f16d31ce162005
Younes Talibi <younessetalibi11@gmail.com>
2024-08-21 12:58:24 +0100

    update

--- contentScript.js
+++ contentScript.js
@@ -1,20 +1,23 @@
 function checkForAdSkipButton() {
-  const adSkipButton1 = document.querySelector(".ytp-ad-skip-button");
-  const adSkipButton2 = document.querySelector(".ytp-ad-skip-button-modern");
-  const adSkipButton3 = document.querySelector('button[id^="skip-button:"]');
-  if (adSkipButton1) {
-    adSkipButton1.click();
-  } else if (adSkipButton2) {
-    adSkipButton2.click();
-  } else if (adSkipButton3) {
-    adSkipButton3.click();
-  }
+    const adSkipButton1 = document.querySelector(".ytp-ad-skip-button");
+    const adSkipButton2 = document.querySelector(".ytp-ad-skip-button-modern");
+    const adSkipButton3 = document.querySelector('button[id^="skip-button:"]');
+    const adSkipButton4 = document.querySelector(".ytp-skip-ad-button");
+    if (adSkipButton1) {
+        adSkipButton1.click();
+    } else if (adSkipButton2) {
+        adSkipButton2.click();
+    } else if (adSkipButton3) {
+        adSkipButton3.click();
+    } else if (adSkipButton4) {
+        adSkipButton4.click();
+    }
 }
 
-const observer = new MutationObserver(function (mutations) {
-  mutations.forEach(function (mutation) {
-    checkForAdSkipButton();
-  });
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        checkForAdSkipButton();
+    });
 });
 
 // Start observing the document for changes
